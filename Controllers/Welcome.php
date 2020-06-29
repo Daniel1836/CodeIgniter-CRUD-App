29678c6ec89da5522cf0ba02c787b80aa6dfb40c
Daniel Klein <daniel.klein@videotron.ca>
2020-06-29 13:31:06 -0400

    Update Welcome.php

--- Controllers/Welcome.php
+++ Controllers/Welcome.php
@@ -23,14 +23,14 @@ class Welcome extends CI_Controller {
 			$this->queries->addPost($data);
 			return redirect('http://localhost:8001/welcome');
 
-	}
+  }
 	public function update($id)
   {
 		$this->load->model('queries');
 		$post = $this->queries->getSinglePosts($id);
 		$this->load->view('update', ['post'=>$post]);
 	
-	}
+  }
 
 	public function change($id)
   {
@@ -39,19 +39,19 @@ class Welcome extends CI_Controller {
 		$this->load->model('queries');
 		$this->queries->UpdatePost($data, $id);
 		return redirect('http://localhost:8001/welcome');
-	}
+  }
 
 	public function view($id)
   {
 		$this->load->model('queries');
 		$post = $this->queries->getSinglePosts($id);
 		$this->load->view('view', ['post'=>$post]);
-	}
+  }
 
 	public function delete($id)
   {
 		$this->load->model('queries');
 		 $this->queries->deletePosts($id);
 		 return redirect('http://localhost:8001/welcome');
-	}
+  }
 }
